1d7dcbab8e0660b78742caa0c1d61fed27d3c181
FastEddy1 <edwardrmock@gmail.com>
2022-05-14 03:41:46 -0600

    Started working on logic

---
+++ script.js
@@ -0,0 +1,47 @@
+//Variables
+var timer = "";
+var score = newScore();
+
+//Start Quiz Function
+function takeQuiz(_startQuiz) {
+    var questions = document.querySelector('#questions');
+    document.getElementById('#question').textContent = "";
+
+}
+var answers = document.querySelector('#answers').textContent = "";
+
+//Need ad event listener here for clicking start quiz button 
+function startQuiz(_startQuiz) {
+    document.getElementById("takequiz");
+    document.addEventListener.click(startQuiz);
+};
+function startTimer() {
+    document.addEventListener.click(startQuiz);
+    document.getElementById("");
+}
+
+//Questions
+function questions() {
+var question1 = "Commonly used data types DO not include:";
+var question2 = "The condition in an if/else statement is enclosed with ____";
+var question3 = "Arrays in JavaScript can used to store ____";
+var question4 = "String value must be enclosed within ____ when being assigned to a variable";
+var question5 = "A very useful tool used during development and debugging for printing content to the debugger is ____.";
+
+}
+//Answers
+function answers() {
+var answer1 = "";
+var answer2 = "";
+var answer3 = "";   //Likely going to have to redo these as each question has multiple answers
+var answer4 = "";
+var answer5 = "";
+}
+
+//Correct Answer Logic
+
+//Incorrect Answer Logic
+
+//Score Logic
+
+//Timer Logic
