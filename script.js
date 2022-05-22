e75782188a07dfcfd2008b0373be29f8e3a9950e
FastEddy1 <edwardrmock@gmail.com>
2022-05-22 14:58:37 -0600

    Refactored random questions function and removed unused variables

--- script.js
+++ script.js
@@ -5,16 +5,11 @@ var questionsSelection = [
     'String value must be enclosed within ____ when being assigned to a variable', 
     'A very useful tool used during development and debugging for printing content to the debugger is ____' ];
 
-var questionOneAnswers = ['1. Strings', '2. Booleans', '3. Alerts', '4. Numbers'];
-var questionTwoAnswers = ['1. Quotes', '2. Curly Brackets', '3. Parenthesis', '4. Square Brackets'];
-var questionThreeAnswers = ['1. Numbers and Strings', '2. Other Arrays', '3. Booleans', '4. All Of The Above'];
-var questionFourAnswers = ['1. Commas', '2. Curly Brackets', '3. Quotes', '4. Parenthesis'];
-var questionFiveAnswers = ['1. JavaScript', '2. Terminal/Bash', '3. For Loops', '4. Console.Log'];
-
 var score = 0;
 var correctAnswerCount = 0;
 var incorrectAnswerCount = 0;
 
+//Random Question Generation
 function getQuestion() {
     var firstQuestion = Math.floor(Math.random(0,5) * 5); //Get random question
     var secondQuestion = Math.floor(Math.random(0,5) * 5);
@@ -27,10 +22,10 @@ function getQuestion() {
         questionText = 'Commonly used data types DO not include:';
         console.log(questionText);
         document.getElementById ('questions.question').innerHTML = questionText;
-        document.getElementById ('answer1').innerHTML = (questionOneAnswers.indexOf(0));
-        document.getElementById ('answer2').innerHTML = (questionOneAnswers.indexOf(1));
-        document.getElementById ('answer3').innerHTML = (questionOneAnswers.indexOf(2));
-        document.getElementById ('answer4').innerHTML = (questionOneAnswers.indexOf(3));
+        document.getElementById ('answer1').innerHTML = ('1. Strings');
+        document.getElementById ('answer2').innerHTML = ('2. Booleans');
+        document.getElementById ('answer3').innerHTML = ('3. Alerts');
+        document.getElementById ('answer4').innerHTML = ('4. Numbers');
         return
     }//Show question 1 and possible answers
     
@@ -38,10 +33,10 @@ function getQuestion() {
         questionText = 'The condition in an if/else statement is enclosed with ____';
         console.log(questionText);
         document.getElementById ('questions.question').innerHTML = questionText;
-        document.getElementById ('answer1').innerHTML = (questionTwoAnswers.indexOf(0));
-        document.getElementById ('answer2').innerHTML = (questionTwoAnswers.indexOf(1));
-        document.getElementById ('answer3').innerHTML = (questionTwoAnswers.indexOf(2));
-        document.getElementById ('answer4').innerHTML = (questionTwoAnswers.indexOf(3));
+        document.getElementById ('answer1').innerHTML = ('1. Quotes');
+        document.getElementById ('answer2').innerHTML = ('2. Curly Brackets');
+        document.getElementById ('answer3').innerHTML = ('3. Parenthesis');
+        document.getElementById ('answer4').innerHTML = ('4. Square Brackets');
         return
     }  //Show question 2 and possible answers
 
@@ -49,20 +44,21 @@ function getQuestion() {
         questionText = 'Arrays in JavaScript can be used to store ____.';
         console.log(questionText);
         document.getElementById ('questions.question').innerHTML = questionText;
-        document.getElementById ('answer1').innerHTML = (questionThreeAnswers.indexOf(0));
-        document.getElementById ('answer2').innerHTML = (questionThreeAnswers.indexOf(1));
-        document.getElementById ('answer3').innerHTML = (questionThreeAnswers.indexOf(2));
-        document.getElementById ('answer4').innerHTML = (questionThreeAnswers.indexOf(3));
+        document.getElementById ('answer1').innerHTML = ('1. Numbers and Strings');
+        document.getElementById ('answer2').innerHTML = ('2. Other Arrays');
+        document.getElementById ('answer3').innerHTML = ('3. Booleans');
+        document.getElementById ('answer4').innerHTML = ('4. All Of The Above');
         return 
-    } //Show question 3
+    } //Show question 3 and possible answers
+    
     else if (fourthQuestion === 3) {
         questionText = 'String value must be enclosed within ____ when being assigned to a variable';
         console.log(questionText);
         document.getElementById ('questions.question').innerHTML = questionText;
-        document.getElementById ('answer1').innerHTML = (questionFourAnswers.indexOf(0));
-        document.getElementById ('answer2').innerHTML = (questionFourAnswers.indexOf(0));
-        document.getElementById ('answer3').innerHTML = (questionFourAnswers.indexOf(0));
-        document.getElementById ('answer4').innerHTML = (questionFourAnswers.indexOf(0));
+        document.getElementById ('answer1').innerHTML = ('1. Commas');
+        document.getElementById ('answer2').innerHTML = ('2. Curly Brackets');
+        document.getElementById ('answer3').innerHTML = ('3. Quotes');
+        document.getElementById ('answer4').innerHTML = ('4. Parenthesis');
         return 
     } //Show question 4 and possible answers
 
@@ -70,10 +66,10 @@ function getQuestion() {
         questionText = 'A very useful tool used during development and debugging for printing content to the debugger is ____';
         console.log(questionText);
         document.getElementById ('questions.question').innerHTML = questionText;
-        document.getElementById ('answer1').innerHTML = (questionFiveAnswers.indexOf(0));
-        document.getElementById ('answer2').innerHTML = (questionFiveAnswers.indexOf(0));
-        document.getElementById ('answer3').innerHTML = (questionFiveAnswers.indexOf(0));
-        document.getElementById ('answer4').innerHTML = (questionFiveAnswers.indexOf(0));
+        document.getElementById ('answer1').innerHTML = ('1. JavaScript');
+        document.getElementById ('answer2').innerHTML = ('2. Terminal/Bash');
+        document.getElementById ('answer3').innerHTML = ('3. For Loops');
+        document.getElementById ('answer4').innerHTML = ('4. Console.Log');
         return 
     } //Show question 5 and possible answers
 }
